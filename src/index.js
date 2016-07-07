74e0665d1e36e36d23616c61cc7806c6aa56fa8c
aphilipps <aphilipps9@gmail.com>
2016-07-07 13:01:43 -0400

    Checkpoint before adding extra

--- src/index.js
+++ src/index.js
@@ -2,18 +2,7 @@
 import $ from 'jquery';
 import './style.scss';
 
-// const $ = require('jquery');
-
-// $('#main').html("Page updated");
-
 let num = 0;
-
-
-// setInterval(function addOne() {
-//   $('#main').html(`You've been on this page for ${num} seconds.`);
-//   num += 1;
-// }, 1000);
-
 
 function addAgain() {
   num += 1;
