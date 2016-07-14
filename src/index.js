7f639176c52f0c22c1562cf31a15527d2a61cda4
aphilipps <aphilipps9@gmail.com>
2016-07-14 11:25:15 -0400

    Website built

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 // change require to es6 import style
-import $ from 'jquery';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import youtubeSearch from './youtube-api';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import debounce from 'lodash.debounce';
 import './style.scss';
 
-let num = 0;
 
-function addAgain() {
-  num += 1;
-  $('#main').html(`You've been on this page for ${num} seconds.`);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.search('pixar');
+
+    this.search = debounce(this.search, 300);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+  }
+
+  search(text) {
+    youtubeSearch(text).then(videos => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar id="searchbar" placeholder="search" onSearchChange={text => this.search(text)} />
+        <div id="video-section">
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList onVideoSelect={selectedVideo => this.setState({ selectedVideo })} videos={this.state.videos} />
+        </div>
+      </div>
+    );
+  }
 }
 
-setInterval(addAgain, 1000);
+ReactDOM.render(<App />, document.getElementById('main'));
